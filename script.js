73eb4f1cf9b075f940e9fd10716137a3f61149e9
Smart-Mu2 <smartmu2muthu@gmail.com>
2024-07-31 09:59:50 +0530

    Home and about completed 31.07.2024

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// Explore button
 let blurBg = document.getElementById("blur-bg");
 
 function exploreNav() {
     blurBg.style.display = "block";
+}
+
+// Navbar
+var sideNav = document.querySelector(".side-navbar");
+function sideNavbar() {
+    sideNav.style.left = "0";
+}
+
+function closeSideNavbar() {
+    sideNav.style.left = "-68%";
 }
