61a71bb6f2f15259e5914f1d12fc2d52c178cf89
Smart-Mu2 <smartmu2muthu@gmail.com>
2024-08-03 23:35:55 +0530

    Rooms page completed 03.08.2024

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let blurBg = document.getElementById("blur-bg");
 function exploreNav() {
     blurBg.style.display = "block";
 }
+// ---
 
 // Navbar
 var sideNav = document.querySelector(".side-navbar");
@@ -14,3 +15,61 @@ function sideNavbar() {
 function closeSideNavbar() {
     sideNav.style.left = "-68%";
 }
+// ---
+
+// Rooms
+let iconHeart = document.querySelector(".iconHeart");
+let iconHeartFill = document.querySelector(".iconHeartFill");
+
+iconHeart.addEventListener('click', likeHeart);
+
+function likeHeart() {
+    iconHeartFill.style.display = "block";
+    iconHeart.style.display = "none";
+}
+
+iconHeartFill.addEventListener('click', disLikeHeart);
+
+function disLikeHeart() {
+    iconHeartFill.style.display = "none";
+    iconHeart.style.display = "block";
+}
+// ---
+
+let iconHeart1 = document.querySelector("#iconHeart");
+let iconHeartFill1 = document.querySelector("#iconHeartFill");
+
+iconHeart1.addEventListener('click', likeHeart1);
+
+function likeHeart1() {
+    iconHeartFill1.style.display = "block";
+    iconHeart1.style.display = "none";
+}
+
+iconHeartFill1.addEventListener('click', disLikeHeart1);
+
+function disLikeHeart1() {
+    iconHeartFill1.style.display = "none";
+    iconHeart1.style.display = "block";
+}
+// ---
+
+let image1 = document.getElementById("Image1");
+function viewImage1() {
+    image1.style.display = "block";
+}
+function closeView1() {
+    image1.style.display = "none";
+}
+// ---
+
+let image2 = document.getElementById("Image2");
+function viewImage2() {
+    image2.style.display = "block";
+
+}
+function closeView2() {
+    image2.style.display = "none";
+}
+
+
