94aa5b07f4ca6572555b429dbe6d48ca765d1b35
Smart-Mu2 <smartmu2muthu@gmail.com>
2024-08-04 21:41:38 +0530

    All done except contact

--- script.js
+++ script.js
@@ -71,5 +71,16 @@ function viewImage2() {
 function closeView2() {
     image2.style.display = "none";
 }
+// ---
 
-
+// Gallery
+document.addEventListener("click",function(e){
+    if (e.target.classList.contains("gallery-item")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+        
+    }
+})
+// ---
