798afb93b27b09fa32319b4e0af86395315df282
Smart-Mu2 <smartmu2muthu@gmail.com>
2024-08-05 13:01:41 +0530

    All done 05.06.2024

--- script.js
+++ script.js
@@ -84,3 +84,6 @@ document.addEventListener("click",function(e){
     }
 })
 // ---
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
